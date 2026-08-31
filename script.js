a34da0cbb7632ee61e6deab8680860279d9005aa
Med-H2026 <anis.harrak13@gmail.com>
2026-08-31 13:28:02 +0300

    Add files via upload
    adding contact form + fixing more responsiveness issues

--- script.js
+++ script.js
@@ -15,3 +15,49 @@ const swiper = new Swiper('.swiper', {
   effect: 'fade',
   fadeEffect: { crossFade: true }
 });
+
+// PRODUCT FILTER
+const tabBtns = document.querySelectorAll('.tab-btn');
+const materialCards = document.querySelectorAll('.material-card');
+
+tabBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    // 1. Remove active from all buttons
+    tabBtns.forEach(b => b.classList.remove('active'));
+    // 2. Add active to clicked button
+    btn.classList.add('active');
+
+    const filter = btn.getAttribute('data-filter');
+
+    // 3. Show/Hide cards
+    materialCards.forEach(card => {
+      if(filter === 'all' || card.dataset.category === filter){
+        card.style.display = 'flex'; // show
+      } else {
+        card.style.display = 'none'; // hide
+      }
+    });
+  });
+});
+
+// BACK TO TOP BUTTON
+const backToTopBtn = document.querySelector('.back-to-top');
+
+// Listem for scroll
+window.addEventListener('scroll', () => {
+    // Show button after scrolling 300px
+    if(window.scrollY > 300){
+        backToTopBtn.classList.add('show');//show button
+    } else {
+        backToTopBtn.classList.remove('show');//hide button
+    }
+});
+
+// Smooth scroll to top when clicked
+backToTopBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // smooth animation
+    });
+});
